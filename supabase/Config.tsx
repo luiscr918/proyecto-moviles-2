b3d2c0d616128d31de7fe62db727ef11bac7831b
luiscr918 <lc9107040@gmail.com>
2025-07-09 09:40:25 -0500

    seguridad en credenciales implementada

--- supabase/Config.tsx
+++ supabase/Config.tsx
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-
+import * as credenciales from '../secrets/credenciales'
 // Create a single supabase client for interacting with your database
-export const supabase = createClient('https://gdzrkcziftzctnajiwzx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkenJrY3ppZnR6Y3RuYWppd3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5ODcxNzMsImV4cCI6MjA2NzU2MzE3M30.yst9VGnBiJzJE1kVxAhk2eNG0tIlT7I95Q334KZWIVc')
+export const supabase = createClient(credenciales.URL, credenciales.API_KEY);
