0df411c16b23b56c94ea4f0af54052cde65a96cf
luiscr918 <lc9107040@gmail.com>
2025-07-09 20:13:04 -0500

    config solo ruta

--- supabase/Config.tsx
+++ supabase/Config.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-import * as credenciales from '../secrets/credenciales'
+import { createClient } from "@supabase/supabase-js";
+import * as credenciales from "../secrets/credenciales";
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(credenciales.URL, credenciales.API_KEY);
